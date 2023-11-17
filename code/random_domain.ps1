1f2d66a9b509540dc299d1cf5b77e6a3a27ae491
Jiniyas Shah <72019447+jiniyasshah@users.noreply.github.com>
2023-11-17 11:57:09 +0545

    Update random_domain.ps1

--- code/random_domain.ps1
+++ code/random_domain.ps1
@@ -3,7 +3,8 @@ param(
     [int]$UserCount,
     [int]$GroupCount,
     [int]$LocalAdminCount
-)
+ )
+
 
 $group_names = [System.Collections.ArrayList](Get-Content "data/group_names.txt")
 $first_names = [System.Collections.ArrayList](Get-Content "data/first_names.txt")
@@ -14,51 +15,57 @@ $groups = @()
 $users = @()
 
 # Default UserCount set to 5 (if not set)
-if ($UserCount -eq 0) {
+if ( $UserCount -eq 0 ){
     $UserCount = 5
 }
 
 # Default GroupCount set to 5 (if not set)
-if ($GroupCount -eq 0) {
+if ( $GroupCount -eq 0 ){
     $GroupCount = 1
 }
 
-if ($LocalAdminCount -ne 0) {
+if ( $LocalAdminCount -ne 0){
     $local_admin_indexes = @()
-    while (($local_admin_indexes | Measure-Object).Count -lt $LocalAdminCount) {
-        $random_index = (Get-Random -InputObject (1..($UserCount)) | Where-Object { $local_admin_indexes -notcontains $_ })
+    while (($local_admin_indexes | Measure-Object ).Count -lt $LocalAdminCount){
+        
+        $random_index = (Get-Random -InputObject (1..($UserCount)) | Where-Object { $local_admin_indexes -notcontains $_ } )
         $local_admin_indexes += @( $random_index )
         echo "adding $random_index to local_admin_indexes $local_admin_indexes"
     }
 }
 
+for ( $i = 1; $i -le $GroupCount; $i++ ){
+    $group_name = (Get-Random -InputObject $group_names)
+    $group = @{ "name" = "$group_name" }
+    $groups += $group
+    $group_names.Remove($group_name)
+}
 
-
-for ($i = 1; $i -le $UserCount; $i++) {
+for ( $i = 1; $i -le $UserCount; $i++ ){
     $first_name = (Get-Random -InputObject $first_names)
     $last_name = (Get-Random -InputObject $last_names)
     $password = (Get-Random -InputObject $passwords)
-    $group_name = (Get-Random -InputObject $group_names)
-    $new_user = @{
-        "name" = "$first_name $last_name"
-        "password" = "$password"
-        "groups" = "$group_name"
-    }
 
-    if ($local_admin_indexes -contains $i) {
+    $new_user = @{ `
+        "name"="$first_name $last_name"
+        "password"="$password"
+        "groups" = (Get-Random -InputObject $groups).name
+        }
+
+    if ( $local_admin_indexes | Where { $_ -eq $i  } ){
         echo "user $i is local admin"
         $new_user["local_admin"] = $true
     }
 
     $users += $new_user
-    $groups += "$group_name"
+
     $first_names.Remove($first_name)
     $last_names.Remove($last_name)
     $passwords.Remove($password)
-}   $group_names.Remove($group_name)
+}
 
-ConvertTo-Json -InputObject @{
-    "domain" = "zero.com"
-    "groups" = $groups
-    "users" = $users
-} | Out-File $OutputJSONFile
+ConvertTo-Json -InputObject @{ 
+    "domain"= "zero.com"
+    "groups"=$groups
+    "users"=$users
+} | Out-File $OutputJSONFile 
